28296dec5b01daf96d4c5166103ecab4c3040e71
ConnerCaprio <doublec1997@gmail.com>
2021-03-13 16:31:25 -0500

    initial commit for new branch

--- backend/app.js
+++ backend/app.js
@@ -49,14 +49,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use("/", express.static(path.join(__dirname, "angular")));
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers',
    'Origin, X-Requested-With, Content-Type, Accept');
    res.setHeader('Access-Control-Allow-Methods',
     'GET, POST, PATCH, DELETE, OPTIONS');
   next();
-});
+}); */
 
 
 
